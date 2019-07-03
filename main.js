514d03aee19923986c34142c75045cf8a1afc5b5
Pranshu Maheshwari <32036787+pranshumaheshwari@users.noreply.github.com>
2019-07-03 08:42:06 +0530

    New investment

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ const alternateURL = `https://www.quandl.com/api/v3/datasets/AMFI/146127.json`;
 
 var value = document.getElementById("value");
 var details = document.getElementById("details");
-const units = [1473.4770, 1428.5710, 979.4320];
+const units = [1473.4770, 1428.5710, 979.4320, 1990.0500];
 const totalUnits = units.reduce((a, b) => a + b);
-const startDate = [new Date("March 10, 2019"), new Date("May 28, 2019"), new Date("June 06, 2019")];
-const startNAV = [10.18, 10.50, 10.21];
+const startDate = [new Date("March 10, 2019"), new Date("May 28, 2019"), new Date("June 06, 2019"), new Date("July 02, 2019")];
+const startNAV = [10.18, 10.50, 10.21, 10.05];
 const startValue = Math.round(startNAV.reduce((r, a, i) => r + a * units[i], 0));
 
 axios.get(URL)
